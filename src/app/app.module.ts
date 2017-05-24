dd2294de3d73839d5dee607fa2367419a9b2fc85
Dylan Nguyen <Dnguye92@yahoo.com>
2017-05-23 22:46:09 -0700

    adding comments for header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeListComponent } from './recipeBook/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipeBook/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipeBook/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shoppingList/shoppingList-Components/shoppingList.component';
+import { ShoppingListEditComponent } from './shoppingList/shoppingListEdit/shoppingListEdit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+//typescript class
+import { Component } from '@angular/core'
+
+//inside of Component is the html selector
+//app-header, the view - header.component.html
+//allocated css file for header.component.html files
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+
+export class HeaderComponent {
+
+}
