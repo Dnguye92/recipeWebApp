c19e905e735f847d0d3e768aba8ac03ff4007b98
Dylan Nguyen <Dnguye92@yahoo.com>
2017-05-24 19:38:33 -0700

    used string interpolation to inject recipe object into the recipe-list.html file

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// we are importing our class 'Recipe' from recipe.model.ts
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  // creating an array of Recipe objects - coming from our Recipe class
+  recipes: Recipe[] = [
+    // we are creating a new instance of the Recipe class
+    // executing this as a method - we are calling the constructor
+    new Recipe('testRecipe', 'this is a test', 'http://pngimg.com/uploads/sushi/sushi_PNG9266.png?i=1')
+  ];
   constructor() { }
 
   ngOnInit() {
