d8b24ace2981223a7be06cb5a4f3c6973eba2be2
Dylan Nguyen <Dnguye92@yahoo.com>
2017-05-24 18:21:46 -0700

    adding navbar within the header component that will be injected into the app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//import custom components here with the
+//file path to the component.ts file
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipeListComponent } from './recipeBook/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipeBook/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipeBook/recipe-detail/recipe-detail.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { ShoppingListComponent } from './shoppingList/shoppingList-Components/shoppingList.component';
 import { ShoppingListEditComponent } from './shoppingList/shoppingListEdit/shoppingListEdit.component';
 
+//include custom made components
+//inside the declarations array
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    RecipesComponent,
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,9 @@
-//typescript class
-import { Component } from '@angular/core'
+// typescript class
+import { Component } from '@angular/core';
 
-//inside of Component is the html selector
-//app-header, the view - header.component.html
-//allocated css file for header.component.html files
+// inside of Component is the html selector
+// app-header, the view - header.component.html
+// allocated css file for header.component.html files
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
